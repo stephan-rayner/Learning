fa0bb3498275fd66632ad8f7e9f88392c6ed5cbb
Stephan Rayner <stephan.rayner@gmail.com>
2018-01-27 14:34:39 -0800

    Adding folder covering explorations if web based c

---
+++ c-web/README.md
@@ -0,0 +1,3 @@
+# Can you write API servers in C
+
+The answer is yes but perhaps a better question is should you here are some options to do just that.
---
+++ c-web/kore/app/src/app.c
@@ -0,0 +1,103 @@
+#include <kore/kore.h>
+#include <kore/http.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+
+char** str_split(char* a_str, const char a_delim)
+{
+    char** result    = 0;
+    size_t count     = 0;
+    char* tmp        = a_str;
+    char* last_comma = 0;
+    char delim[2];
+    delim[0] = a_delim;
+    delim[1] = 0;
+
+    /* Count how many elements will be extracted. */
+    while (*tmp)
+    {
+        if (a_delim == *tmp)
+        {
+            count++;
+            last_comma = tmp;
+        }
+        tmp++;
+    }
+
+    /* Add space for trailing token. */
+    count += last_comma < (a_str + strlen(a_str) - 1);
+
+    /* Add space for terminating null string so caller
+       knows where the list of returned strings ends. */
+    count++;
+
+    result = malloc(sizeof(char*) * count);
+
+    if (result)
+    {
+        size_t idx  = 0;
+        char* token = strtok(a_str, delim);
+
+        while (token)
+        {
+            assert(idx < count);
+            *(result + idx++) = strdup(token);
+            token = strtok(0, delim);
+        }
+        assert(idx == count - 1);
+        *(result + idx) = 0;
+    }
+
+    return result;
+}
+
+
+int	page(struct http_request *);
+
+int page(struct http_request *req)
+{
+	//http_response(req, 200, NULL, 0);
+    printf("%s\n", req->query_string);
+
+    char** params;
+    params = str_split(req->query_string, '&');
+
+    if (params)
+    {
+        for (int i = 0; *(params + i); i++)
+        {
+            printf("params=[%s]\n", *(params + i));
+            free(*(params + i));
+        }
+        printf("\n");
+        free(params);
+    }
+
+    http_response(req, 200, "Hello world", 11);
+	return (KORE_RESULT_OK);
+}
+
+/*
+char months[] = "JAN,FEB,MAR,APR,MAY,JUN,JUL,AUG,SEP,OCT,NOV,DEC";
+    char** tokens;
+
+    printf("months=[%s]\n\n", months);
+
+    tokens = str_split(months, ',');
+
+    if (tokens)
+    {
+        int i;
+        for (i = 0; *(tokens + i); i++)
+        {
+            printf("month=[%s]\n", *(tokens + i));
+            free(*(tokens + i));
+        }
+        printf("\n");
+        free(tokens);
+    }
+
+    return 0;
+*/
