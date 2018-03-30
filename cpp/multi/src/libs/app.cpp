938f59d74c6045f60e2a5aea3ecb4803fb83d562
Stephan Rayner <stephan.rayner@gmail.com>
2018-03-30 15:45:53 -0700

    Now it is prettier... or something.

--- cpp/multi/src/libs/app.cpp
+++ cpp/multi/src/libs/app.cpp
@@ -3,9 +3,12 @@
 #include <vector>
 
 void run(std::vector<int> c) {
-    for (int i = 0; i < c.size(); i++)
-    {
+    for (int i = 1; i <= c.size(); i++) {
         c[i] = i * i;
-        std::cout << c[i] << std::endl;
+        std::cout << i << " * " << i << " = " << c[i] << " | ";
+        if ( i % 3 == 0) {
+            std::cout << std::endl;
+        }
     }
+    std::cout << std::endl;
 }
