0ccb78faa144e7fa01056f6991c74d9f2f421e81
Stephan Rayner <stephan.rayner@gmail.com>
2018-03-30 16:42:04 -0700

    Fun with command line args

--- cpp/multi/src/main.cpp
+++ cpp/multi/src/main.cpp
@@ -1,13 +1,24 @@
 #include <iostream>
 #include <vector>
+#include <sstream> // for std::stringstream
 #include "libs/app.h"
 
-int main() {
+int main(int argc, char *argv[]) {
     int len;
-    std::cout << "Give me a number plz: ";
-    std::cin >> len;
+
+    if (argc > 1) {
+        std::stringstream geek(argv[1]);
+        std::cout << "argc = " << argc << std::endl;
+        std::cout << argv[1] << std::endl;
+        geek >> len;
+
+    } else {
+        std::cout << "Give me a number plz: ";
+        std::cin >> len;
+    }
+
     std::vector<int> c;
     c.resize(len);
     run(c);
-    return 0;
+    exit(0);
 }
